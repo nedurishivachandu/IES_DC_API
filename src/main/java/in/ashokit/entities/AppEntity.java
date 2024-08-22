fd61bc1124c711fea38a602c3b5c47e8230691b0
ShivaChandu <nedurishivachandueee@gmail.com>
2024-08-22 12:00:50 +0530

    first commit

---
+++ src/main/java/in/ashokit/entities/AppEntity.java
@@ -0,0 +1,91 @@
+package in.ashokit.entities;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="IES_APPS")
+public class AppEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long caseNum;
+	
+	private String fullName;
+	private String email;
+	private String gender;
+	private LocalDate dob;
+	private Long phno;
+	private Long ssn;
+	
+	private UserEntity user;
+
+	public Long getCaseNum() {
+		return caseNum;
+	}
+
+	public void setCaseNum(Long caseNum) {
+		this.caseNum = caseNum;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public LocalDate getDob() {
+		return dob;
+	}
+
+	public void setDob(LocalDate dob) {
+		this.dob = dob;
+	}
+
+	public Long getPhno() {
+		return phno;
+	}
+
+	public void setPhno(Long phno) {
+		this.phno = phno;
+	}
+
+	public Long getSsn() {
+		return ssn;
+	}
+
+	public void setSsn(Long ssn) {
+		this.ssn = ssn;
+	}
+
+	public UserEntity getUser() {
+		return user;
+	}
+
+	public void setUser(UserEntity user) {
+		this.user = user;
+	}
+
+}
